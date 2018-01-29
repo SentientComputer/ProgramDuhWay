359e6693e0d1d118f706611bf030dd0c4c728769
Christian Purcell <purcell1@graceland.edu>
2018-01-28 23:18:54 -0600

    website update

---
+++ Data Structures/Physician's Office Sim/ChristianPurcell'sPhysicianSim.py
@@ -0,0 +1,20 @@
+##tsw = Time spent waiting
+##tse = Time spent examining
+##Christian Purcell's simulation of a physician's office
+
+def main():
+    class Docs():
+        def __init__(self):
+
+    class TriNur():
+        def __init__(self):
+
+    class WaitRoom():
+        def __init__(self, tsw):
+            self.tsw = tsw
+    class Sickie():
+        def __init__ (self)
+        
+    class ExamRoom():
+        def __init__(self, tse):
+            self.tse = tse
