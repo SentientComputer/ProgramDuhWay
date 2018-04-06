02946611a02139c8ce66ea433827af1e05afddc0
SentientComputer <35306209+SentientComputer@users.noreply.github.com>
2018-04-06 12:14:38 -0500

    Add files via upload

---
+++ JavaPractice 1/info.js
@@ -0,0 +1,6 @@
+var name = "Christian Antiquity Purcell";
+
+document.write("<span id ='footer'>Christian's Website</span><br />");
+document.write("<span id ='Copyright'>©2018 Christian Antiquity Purcell</span><br />");
+document.write("<span id ='Modified'>April 6 2018</span><br />");
+document.write("<span id ='Retrieved'>April 6 2018</span><br />");
